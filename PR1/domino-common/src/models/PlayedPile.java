8ca49e82ae861415f370f2d4eda51417ee29e4cd
hermetico <yojmarinv@gmail.com>
2014-03-02 18:58:59 +0100

    añadidas funciones: matchLeft matchRight matchSide pushSide

--- PR1/domino-common/src/models/PlayedPile.java
+++ PR1/domino-common/src/models/PlayedPile.java
@@ -1,6 +1,8 @@
 package models;
 
 public class PlayedPile extends Catalog {
+	static final char RIGHTSIDE = 'R';
+	static final char LEFTSIDE = 'L';
 	
 	public PlayedPile() {
 		super();
@@ -21,6 +23,51 @@ public class PlayedPile extends Catalog {
 	
 	public void pushRight (Piece piece) {
 		this.pieces.addLast(new Piece(piece));
+		
+	}
+	
+	public void pushSide(Piece piece, char side){
+		side = Character.toUpperCase(side);
+		if(side == LEFTSIDE)pushLeft(piece);
+		else pushRight(piece); 
+	}
+	
+	public void pushSide(Piece piece, String side){
+		pushSide(piece, side.charAt(0));
+	}
+	/**
+	 * Returns if the specified piece matches with the pieces on the right side of the pile
+	 * @param piece
+	 * @return
+	 */
+	public boolean matchRight(Piece piece){
+		return this.pieces.peekLast().matches(piece, RIGHTSIDE);
+	}
+	/**
+	 * Returns if the specified piece matches with the pieces on the left side of the pile
+	 * @param piece
+	 * @return
+	 */
+	public boolean matchLeft(Piece piece){
+		return this.pieces.peekFirst().matches(piece, LEFTSIDE);
+	}
+	/**
+	 * Returns if the specified piece matches with the pieces on the specified side of the pile
+	 * @param piece
+	 * @return
+	 */
+	public boolean matchSide(Piece piece, char side){
+		side = Character.toUpperCase(side);
+		return (side == LEFTSIDE) ? matchLeft(piece) : matchRight(piece);
+		
+	}
+	/**
+	 * Returns if the specified piece matches with the pieces on the specified side of the pile
+	 * @param piece
+	 * @return
+	 */
+	public boolean matchSide(Piece piece, String side){
+		return matchSide(piece, side.charAt(0));
 		
 	}
 }
