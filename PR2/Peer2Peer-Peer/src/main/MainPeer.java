3ac2b0c2462341c51678d4aa10b30aeca34273e3
dennis <dennis@maikel>
2014-03-31 10:44:49 +0200

    dialog window que preguntar por el username

--- PR2/Peer2Peer-Peer/src/main/MainPeer.java
+++ PR2/Peer2Peer-Peer/src/main/MainPeer.java
@@ -2,10 +2,24 @@ package main;
 
 import interficie.server.ChatDaemonInterface;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
 
 import peer.ChatPeer;
 import views.DialogWindow;
@@ -32,22 +46,25 @@ public class MainPeer {
 			host = args[0];
 		}
 
-		new MainPeer(host, port, "Hermetico");
+		new MainPeer(host, port);
 
 	}
 
-	public MainPeer(String host, int port, String nombre) {
+	public MainPeer(String host, int port) {
+
 		this.host = host;
 		this.port = port;
-		this.nombre = nombre;
 
 		String urlRegistro = "rmi://" + host + ":" + port + "/" + serverName;
+
+		UserNameDialog(urlRegistro);
+		
 		try {
 			ChatDaemonInterface chatServer = (ChatDaemonInterface) Naming
 					.lookup(urlRegistro);
 			System.out.println("Server encontrado");
 
-			client = new ChatPeer(chatServer, nombre);
+			client = new ChatPeer(chatServer, this.nombre);
 			chatServer.registerPeer(nombre, client);
 			client.go();
 
@@ -59,4 +76,31 @@ public class MainPeer {
 
 	}
 
+	private void UserNameDialog(String url) {
+		// pop-up para preguntar el username
+		DialogWindow dialog = new DialogWindow(this, url);
+
+		Thread thr = new Thread(dialog);
+		thr.start();
+
+		synchronized (this) {
+			while (!dialog.isReady()) {
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// Nos quedamos con el username insertado por el cliente
+		this.nombre = dialog.getUserName();
+
+		dialog.dispose();
+		// Si se ha dado al boton de cancelar se cierra todo el programa cliente
+		if (this.nombre == null)
+			System.exit(0);
+
+	}
 }
--- PR2/Peer2Peer-Peer/src/views/DialogWindow.java
+++ PR2/Peer2Peer-Peer/src/views/DialogWindow.java
@@ -1,23 +1,47 @@
 package views;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
-public class DialogWindow extends JDialog {
+import main.MainPeer;
+import java.awt.Font;
+
+public class DialogWindow extends JDialog implements Runnable {
 	private JTextField textField;
+	private JLabel userLabel;
+	private JButton okButton;
 
-	public DialogWindow() {
+	private String urlRegistro;
+	private MainPeer context;
+	private boolean ready;
+	private String userName;
+
+	public DialogWindow(MainPeer mp, String url) {
+
+		this.ready = false;
+		this.urlRegistro = url;
+		this.context = mp;
 
 		initialize();
+
+	}
+
+	@Override
+	public void run() {
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setVisible(true);
 	}
@@ -28,40 +52,128 @@ public class DialogWindow extends JDialog {
 	private void initialize() {
 
 		setBounds(100, 100, 496, 248);
-		getContentPane().setLayout(new BorderLayout());
-		
+		getContentPane().setLayout(new BorderLayout(0, 0));
+
 		{
+			// BUTTON PANE
 			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			buttonPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 			{
-				JButton okButton = new JButton("OK");
+				okButton = new JButton("OK");
+				okButton.setEnabled(false);
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
+				okButton.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						userName = textField.getText();
+						tanca();
+					}
+				});
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setHorizontalAlignment(SwingConstants.RIGHT);
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
+				cancelButton.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						userName = null;
+						tanca();
+					}
+				});
 			}
 		}
-		
 		{
-			JLabel userLabel = new JLabel("user name:");
-			userLabel.setHorizontalAlignment(SwingConstants.TRAILING);
-			getContentPane().add(userLabel, BorderLayout.WEST);
-			userLabel.setToolTipText("Enter a user name to join the chat");
+			// INFO PANE
+			JPanel infoPane = new JPanel();
+			getContentPane().add(infoPane);
+			infoPane.setLayout(null);
 
-		}
-		
-		{
-			textField = new JTextField();
-			getContentPane().add(textField, BorderLayout.CENTER);
-			textField.setColumns(10);
-			textField.setSize(50,50);
+			{
+				userLabel = new JLabel("USER NAME:");
+				userLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+				userLabel.setBounds(200, 100, 80, 20);
+				infoPane.add(userLabel);
+				userLabel.setHorizontalAlignment(SwingConstants.CENTER);
+				userLabel.setToolTipText("Enter a user name to join the chat");
+
+			}
+
+			{
+				textField = new JTextField();
+				textField.setLocation(175, 131);
+				textField.setHorizontalAlignment(SwingConstants.LEFT);
+				userLabel.setLabelFor(textField);
+				infoPane.add(textField);
+				textField.setColumns(10);
+				textField.setSize(130, 20);
+				textField.getDocument().addDocumentListener(
+						new DocumentListener() {
+
+							@Override
+							public void removeUpdate(DocumentEvent e) {
+								changed();
+							}
+
+							@Override
+							public void insertUpdate(DocumentEvent e) {
+								changed();
+							}
+
+							@Override
+							public void changedUpdate(DocumentEvent e) {
+								changed();
+							}
+
+							public void changed() {
+								if (textField.getText().equals("")) {
+									okButton.setEnabled(false);
+								} else {
+									okButton.setEnabled(true);
+								}
+
+							}
+						});
+
+			}
+
+			JLabel infoServerLabel = new JLabel();
+			infoServerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			infoServerLabel.setText("Attempting to connect...");
+			infoServerLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+			infoServerLabel.setBounds(116, 11, 247, 42);
+			infoPane.add(infoServerLabel);
+
+			JLabel label = new JLabel();
+			label.setVerticalAlignment(SwingConstants.TOP);
+			label.setText(this.urlRegistro);
+			label.setHorizontalAlignment(SwingConstants.CENTER);
+			label.setFont(new Font("Tahoma", Font.BOLD, 14));
+			label.setBounds(116, 60, 247, 42);
+			infoPane.add(label);
 		}
 
 	}
 
+	private void tanca() {
+		synchronized (context) {
+			ready = true;
+			context.notifyAll();
+		}
+	}
+
+	public boolean isReady() {
+		return ready;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
 }
